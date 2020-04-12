dd9dc7186d8c6a92bce53b9096b596945fc01b4f
Alexander Cotignola <acotigno@umic@umich.edu>
2020-04-12 14:39:30 -0400

    Put all restaurant data into one table

--- yelp_api.py
+++ yelp_api.py
@@ -7,69 +7,40 @@ import pandas as pd
 def setUpDatabase(data,location):
     conn = sqlite3.connect('restaurants.sqlite')
     cur = conn.cursor()
-    if location == "Central Park":
-        cur.execute('''CREATE TABLE IF NOT EXISTS Central_Park (restaurant_id TEXT PRIMARY KEY, restaurant_name TEXT, address TEXT, lat REAL, long REAL, rating REAL, price TEXT,category TEXT,review_count INTEGER)''')
-        restaurant_list = []
-        for business in data["businesses"]:
-            restaurant_id = business['id']
-            name = business['name']
-            address = business['location']['address1']
-            lat = business['coordinates']['latitude']
-            longitude = business['coordinates']['longitude']
-            rating = business['rating']
-            if 'price' in business:
-                price = business['price']
+    cur.execute('''CREATE TABLE IF NOT EXISTS Restaurants (restaurant_id TEXT PRIMARY KEY, restaurant_name TEXT, location TEXT, address TEXT, lat REAL, long REAL, rating REAL, price TEXT,category TEXT,review_count INTEGER)''')
+    for business in data["businesses"]:
+        restaurant_id = business['id']
+        name = business['name']
+        location = location
+        address = business['location']['address1']
+        lat = business['coordinates']['latitude']
+        longitude = business['coordinates']['longitude']
+        rating = business['rating']
+        if 'price' in business:
+            if business['price'] == "$":
+                price = 1.0
+            elif business['price'] == "$$":
+                price = 2.0
+            elif business['price'] == "$$$":
+                price = 3.0
             else:
-                price = '$$$$'
-            category = business['categories'][0]['title']
-            review_count = business['review_count']
-            business_tuple = (restaurant_id,name,address,lat,longitude,rating,price,category,review_count)
-            restaurant_list.append(business_tuple)
-            for i in range(len(restaurant_list)):
-                cur.execute("INSERT OR IGNORE INTO Central_Park (restaurant_id,restaurant_name,address,lat,long,rating,price,category,review_count) VALUES (?,?,?,?,?,?,?,?,?)",(restaurant_list[i][0],restaurant_list[i][1],restaurant_list[i][2],restaurant_list[i][3],restaurant_list[i][4],restaurant_list[i][5],restaurant_list[i][6],restaurant_list[i][7],restaurant_list[i][8]))
+                price = 4.0
+        else:
+            price = 4.0
+        category = business['categories'][0]['title']
+        review_count = business['review_count']
+        cur.execute("INSERT OR IGNORE INTO Restaurants (restaurant_id,restaurant_name,location,address,lat,long,rating,price,category,review_count) VALUES (?,?,?,?,?,?,?,?,?,?)",(restaurant_id,name,location,address,lat,longitude,rating,price,category,review_count))
         conn.commit()
-    elif location == "Times Square":
-        cur.execute('''CREATE TABLE IF NOT EXISTS Times_Square (restaurant_id TEXT PRIMARY KEY, restaurant_name TEXT, address TEXT, lat REAL, long REAL, rating REAL, price TEXT,category TEXT,review_count INTEGER)''')
-        restaurant_list = []
-        for business in data["businesses"]:
-            restaurant_id = business['id']
-            name = business['name']
-            address = business['location']['address1']
-            lat = business['coordinates']['latitude']
-            longitude = business['coordinates']['longitude']
-            rating = business['rating']
-            if 'price' in business:
-                price = business['price']
-            else:
-                price = '$$$$'
-            category = business['categories'][0]['title']
-            review_count = business['review_count']
-            business_tuple = (restaurant_id,name,address,lat,longitude,rating,price,category,review_count)
-            restaurant_list.append(business_tuple)
-            for i in range(len(restaurant_list)):
-                cur.execute("INSERT OR IGNORE INTO Times_Square (restaurant_id,restaurant_name,address,lat,long,rating,price,category,review_count) VALUES (?,?,?,?,?,?,?,?,?)",(restaurant_list[i][0],restaurant_list[i][1],restaurant_list[i][2],restaurant_list[i][3],restaurant_list[i][4],restaurant_list[i][5],restaurant_list[i][6],restaurant_list[i][7],restaurant_list[i][8]))
-        conn.commit()
-    else:
-        cur.execute('''CREATE TABLE IF NOT EXISTS World_Trade_Center (restaurant_id TEXT PRIMARY KEY, restaurant_name TEXT, address TEXT, lat REAL, long REAL, rating REAL, price TEXT,category TEXT,review_count INTEGER)''')
-        restaurant_list = []
-        for business in data["businesses"]:
-            restaurant_id = business['id']
-            name = business['name']
-            address = business['location']['address1']
-            lat = business['coordinates']['latitude']
-            longitude = business['coordinates']['longitude']
-            rating = business['rating']
-            if 'price' in business:
-                price = business['price']
-            else:
-                price = '$$$$'
-            category = business['categories'][0]['title']
-            review_count = business['review_count']
-            business_tuple = (restaurant_id,name,address,lat,longitude,rating,price,category,review_count)
-            restaurant_list.append(business_tuple)
-        for i in range(len(restaurant_list)):
-            cur.execute("INSERT OR IGNORE INTO World_Trade_Center (restaurant_id,restaurant_name,address,lat,long,rating,price,category,review_count) VALUES (?,?,?,?,?,?,?,?,?)",(restaurant_list[i][0],restaurant_list[i][1],restaurant_list[i][2],restaurant_list[i][3],restaurant_list[i][4],restaurant_list[i][5],restaurant_list[i][6],restaurant_list[i][7],restaurant_list[i][8]))
-        conn.commit()
+
+def getCategories():
+    conn = sqlite3.connect('restaurants.sqlite')
+    cur = conn.cursor()
+    cur.execute('SELECT category FROM Restaurants')
+    database_list = cur.fetchall()
+    category_list = []
+    for item in database_list:
+        category_list.append(item[0])
+    print(category_list)
     
 def getData(lat,long_,location,offset):
     api_key = 'ViVcJp0uJf0RJ32VQJKZIDhWxRfSS08elfK4fX31-s8BuL2nUT8h-b50QsPDHbWDOmt3NqAPu8e0rjPhVoupai8KwCGLa6EZmR4nZARu3P2g6k_JpT9-CxQXfguNXnYx'
@@ -88,6 +59,7 @@ def getData(lat,long_,location,offset):
     time.sleep(5)
 # getData('40.7829','-73.9654','Central Park')
 # getData('40.7580','-73.9855','Times Square')
-for i in range(1,11):
-    offset = 20*i
-    getData('40.7580','-73.9855','Times Square',offset)
+if __name__ == "__main__":
+    # offset = input("Enter an offset: ")
+    # getData('40.7127','-74.0134','World Trade Center',offset)
+    getCategories()
